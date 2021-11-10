e5e687cc9d2602dabd1a2820a4fe05aa8b5f677c
momousa420 <mimity34@gmail.com>
2021-11-11 02:34:51 +0800

    判斷式、迴圈、陣列

---
+++ Asia_Test_1001/Assets/Scripts/LearnArray.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ¾Ç²ß°}¦C Array
+/// </summary>
+
+public class LearnArray : MonoBehaviour
+{
+
+}
+
--- Asia_Test_1001/Assets/Scripts/LearnOperator.cs
+++ Asia_Test_1001/Assets/Scripts/LearnOperator.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 /// 學習運算子
 /// 1.指定 =
 /// 2.數學 + - * /  餘數:%  遞增:++  遞減:--
-/// 3.比較
-/// 4.邏輯
+/// 3.比較 > < >= <= == !=
+/// 4.邏輯 &&(並且)  ||(或者)  顛倒 !
 /// </summary>
 public class LearnOperator : MonoBehaviour
 {
@@ -18,11 +18,17 @@ public class LearnOperator : MonoBehaviour
     public float numberA = 20;
     public float numberB = 3;
     public int numberC = 30;
+    public int numberD = 5;
+
+    public int numberE = 9;
+    public int numberF = 1;
+
     #endregion
 
-    #region  數學運算子
+    #region  
     private void Start()
     {
+        #region 數學運算子
         print("加法" + (numberA + numberB));
         print("減法" + (numberA - numberB));
         print("乘法" + (numberA * numberB));
@@ -34,6 +40,41 @@ public class LearnOperator : MonoBehaviour
         numberC--;                           //遞減
         print("numberC減1：" + numberC);
 
+        numberD = numberD + 10;
+        print("numberD 加 10：" + numberD);
+        numberD += 10;                       //遞增指定 + - * / %
+        print("numberD 加 10：" + numberD);
+        #endregion
+
+        #region 比較運算子
+        //作用：比較兩個值，並取得布林值結果
+        print("E > F：" + (numberE > numberF));
+        print("E < F：" + (numberE < numberF));
+        print("E >= F：" + (numberE >= numberF));
+        print("E <= F：" + (numberE <= numberF));
+        print("E == F：" + (numberE == numberF));
+        print("E != F：" + (numberE != numberF));
+
+        #endregion
+
+        #region 邏輯運算子
+        //作用：比較兩個布林值，並取得布林值結果
+        //並且：只要有一個false，結果為false
+        print("T && T：" + (true && true));
+        print("T && F：" + (true && false));
+        print("F && T：" + (false && true));
+        print("F && F：" + (false && false));
+
+        //或著：只要有一個true，結果為true
+        print("T || T：" + (true || true));
+        print("T || F：" + (true || false));
+        print("F || T：" + (false || true));
+        print("F || F：" + (false || false));
+
+        //顛倒：將布林值給為相反
+        print("!true：" + (!true));
+        print("!false：" + (!false));
+        #endregion
     }
     #endregion
 }
