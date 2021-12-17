4941483de8091033e804d144ab606f8d5ea3e8fc
momousa420 <90890559+momousa420@users.noreply.github.com>
2021-12-17 14:46:39 +0800

    12.17_2
    中存2

---
+++ 期中作業/Assets/Scripts/NPC.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("對話資料")]
+    public DataDialogue dataDialogue;
+    [Header("對話系統")]
+    public DialogueSystem dialogueSystem;
+
+}
