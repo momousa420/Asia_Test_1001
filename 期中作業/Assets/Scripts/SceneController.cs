aa0abb20696753ead4508a95861739841a388c96
momousa420 <90890559+momousa420@users.noreply.github.com>
2022-01-07 15:59:30 +0800

    220107
    阿巴阿巴

---
+++ 期中作業/Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        print("Ãö³¬¹CÀ¸");
+    }
+}
